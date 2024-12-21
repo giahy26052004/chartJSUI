75680a83d9fcbc4affa771b23f03b8f72061e157
=giahy26052004 <=giahy26052004@gmail.com>
2024-12-21 23:00:02 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from "react";
+
+import "./App.css";
+import LineChart from "./components/LineChart";
+
+function App() {
+  return (
+    <div className="App">
+      <LineChart />
+    </div>
+  );
+}
+
+export default App;
